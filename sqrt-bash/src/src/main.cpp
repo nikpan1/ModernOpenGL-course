e5424256821e602639d78d2437fed68baf5f191e
nikpan1 <96376932+nikpan1@users.noreply.github.com>
2023-06-30 14:15:46 +0200

    Section 2 Part 3
    Translation, rotation, scaling

--- sqrt-bash/src/src/main.cpp
+++ sqrt-bash/src/src/main.cpp
@@ -2,20 +2,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
 #include <GL\glew.h>
 #include <GLFW\glfw3.h>
-#include <glm/mat4x4.hpp>
+
+#include <glm\mat4x4.hpp>
+#include <glm\glm.hpp>
+#include <glm\gtc\matrix_transform.hpp>
+#include <glm\gtc\type_ptr.hpp>
 
 
 const GLint WIDTH = 800, HEIGHT = 600;
-
+const float toRadians = 3.14159265f / 180.f;
 GLuint VAO, VBO, shader;
 
 bool direction = true;
 float triOffset = 0.0f;
-float trMaxOffset = 0.7f;
+float triMaxOffset = 0.7f;
 float triIncrement = 0.0005f;
-GLuint uniformXMove;
+float curlAngle = 0.0f;
+
+GLuint uniformModel;
 
 
 // Vertex Shader code
@@ -23,10 +30,10 @@ static const char* vShader = "                                                \n
 #version 330                                                                  \n\
                                                                               \n\
 layout (location = 0) in vec3 pos;											  \n\
-uniform float xMove;                                                          \n\
+uniform mat4 model;	                                                          \n\
 void main()                                                                   \n\
 {                                                                             \n\
-    gl_Position = vec4(0.4 * pos.x + xMove, 0.4 * pos.y, pos.z, 1.0);		  \n\
+    gl_Position = model * vec4(0.3 * pos.x, 0.3 * pos.y, pos.z, 1.0);		  \n\
 }";
 
 // Fragment Shader
@@ -118,7 +125,7 @@ void CompileShaders() {
 		return;
 	}
 
-	uniformXMove = glGetUniformLocation(shader, "xMove");
+	uniformModel = glGetUniformLocation(shader, "model");
 }
 
 int main() {
@@ -162,18 +169,30 @@ int main() {
 
 	while (!glfwWindowShouldClose(mainWindow)) {
 		glfwPollEvents();
-		
-		if (direction) triOffset += triIncrement;
-		else triOffset -= triIncrement;
-
-		if (abs(triOffset) >= trMaxOffset) direction != direction;
 
 		glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 		
 		glUseProgram(shader);
+		
+		if (direction) triOffset += triIncrement;
+		else triOffset -= triIncrement;
+
+		if (abs(triOffset) >= triMaxOffset) direction = !direction;
+		
+		curlAngle += 0.01f;
+		if (curlAngle >= 360) curlAngle -= 360;
+
+
+		// it's important to initialize an indetity matrix with constructor
+		glm::mat4 model = glm::mat4(1.0f);
+		// the order is important
+		model = glm::translate(model, glm::vec3(triOffset, 0.0f, 0.0f));
+		model = glm::rotate(model, curlAngle * toRadians, glm::vec3(0.0f, 0.0f, 1.0f));
+		model = glm::scale(model, glm::vec3(3.f, 2.f, 1.f));
 	
-		glUniform1f(uniformXMove,triOffset);
+		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
+		
 		glBindVertexArray(VAO);
 		glDrawArrays(GL_TRIANGLES, 0, 3);
 		glBindVertexArray(0);
